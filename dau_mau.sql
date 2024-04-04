6e11163a4648bd8a48fce86a75016f69fd3939cb
Alex ignatenko <31753534+alexignatenko@users.noreply.github.com>
2024-04-04 17:40:54 +0800

    Update dau_mau.sql

--- dau_mau.sql
+++ dau_mau.sql
@@ -1,3 +1,5 @@
+#calculate dau/mau rate
+
 SELECT COUNT(DISTINCT IF(_table_suffix > FORMAT_DATE('%Y%m%d',DATE_SUB('2021-01-31', INTERVAL 1 day)), user_pseudo_id, NULL)) AS DAU,
        COUNT(DISTINCT IF(_table_suffix > FORMAT_DATE('%Y%m%d',DATE_SUB('2021-01-31', INTERVAL 30 day)), user_pseudo_id, NULL)) AS MAU
 FROM `your-project.events_*`
